85954c2ad114ff8702a31362daf164419bfae959
Neghi14 <106474028+neghi14@users.noreply.github.com>
2022-07-27 03:38:10 -0700

    New push

--- main.js
+++
@@ -1,6 +0,0 @@
-const clicker = document.querySelector('.clicker');
-const navBar = document.querySelector('.nav-links');
-
-clicker.addEventListener('click', () => {
-    navBar.classList.toggle('hidden');
-})
